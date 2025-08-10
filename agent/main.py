9bc32996db095c8000237d2a53dd204a42525408
haochend413 <dinghaochen413@gmail.com>
2025-08-10 11:35:00 -0500

    feat: git

--- agent/main.py
+++ agent/main.py
@@ -7,12 +7,10 @@ from db.db import create_db_and_tables, db_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # onstartup, create;
-    print("ffff")
     create_db_and_tables()
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-
 
 app.include_router(db_router, prefix="/db")
